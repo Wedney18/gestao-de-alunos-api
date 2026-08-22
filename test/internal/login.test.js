a92255d924eb504e32a8a3025c9fcb42e91a362f
Wedney <wedney.santos@gmail.com>
2026-08-22 17:46:59 -0300

    Adicionando novos arquivos

--- test/internal/login.test.js
+++ test/internal/login.test.js
@@ -1,6 +1,8 @@
 import request from 'supertest';
-import app from '../src/app.js';
+import app from '../../src/app.js';
 import { expect } from 'chai';
+import * as sinon from 'sinon';
+import authService from '../../src/services/auth.service.js';
 
 describe('Login', () => {
     it('deve retornar 200 quando o usuário e senha forem corretos', async () => {
@@ -37,5 +39,23 @@ describe('Login', () => {
             });
 
         expect(loginResposta.status).to.equal(401);
+    });
+
+    it ('deve retornar 500 quando acontecer algum problema de conexão com o banco de dados', async () => {
+        const authServiceMock = sinon.stub(authService, 'login');
+        authServiceMock.throws(new Error('Erro catastrófico!'));  
+
+        const loginResposta = await request(app)
+            .post('/api/auth/login')
+            .set('Content-Type', 'application/json')
+            .send({
+                email: 'admin@escola.com',
+                senha: 'admin123'
+            });
+
+        expect(loginResposta.status).to.equal(500);
+        expect(loginResposta.body.error).to.equal('Erro interno do servidor.');
+
+        sinon.restore();
     });
 });
